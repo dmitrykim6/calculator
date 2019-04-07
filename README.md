2076b29bbcaaa63e5413480c45a2d4b5bc7828f6
Dmitry <kido.pay@gmail.com>
2019-04-07 10:14:59 +0600

    new version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # calculator
 lessons by DevCIT
+
+        // ошибка при сложении маленьких чисел!
+//        char a = temp.charAt(temp.length() - 2);
+//        char b = temp.charAt(temp.length() - 1);
+//
+//        if(a == '.' && b == '0'){
+//            temp = temp.substring(0, temp.length()-2);
+//        }
+
+1. Отсутствует второе число 
+2. Если РАВНО нажато первым значением
+
+
+
+
+
+
--- src/Model.java
+++ src/Model.java
@@ -4,6 +4,7 @@ class Model {
     String leftValue; // first number
     String rightValue; // second number
     char action; //
+//    int equalsCount; // счетчик повторных нажатий на РАВНО
 
     Model (Viewer viewer){
         this.viewer = viewer;
@@ -21,46 +22,59 @@ class Model {
             rightValue = "";
             action = '@';
 
+
         }else if (value.equals("One")) {
             temp = temp + "1";
+
 
         } else if (value.equals("Two")) {
             temp = temp + "2";
 
+
         }else if (value.equals("Three")) {
             temp = temp + "3";
+
 
         }else if (value.equals("Four")) {
             temp = temp + "4";
 
+
         }else if (value.equals("Five")) {
             temp = temp + "5";
+
 
         }else if (value.equals("Six")) {
             temp = temp + "6";
 
+
         }else if (value.equals("Seven")) {
             temp = temp + "7";
+
 
         }else if (value.equals("Eight")) {
             temp = temp + "8";
 
+
         }else if (value.equals("Nine")) {
             temp = temp + "9";
 
+
         }else if (value.equals("Zero")) {
             temp = temp + "0";
+
 
         }else if(value.equals("Plus")){
             leftValue = temp;
             temp = "";
             action = '+';
+
             return;
 
         }else if(value.equals("Minus")){
             leftValue = temp;
             temp = "";
             action = '-';
+
             return;
 
         }
@@ -75,12 +89,23 @@ class Model {
 
     public void Equal(String leftValue, char action){
 
-        rightValue = temp;
+        if (temp == "" && action =='+'){
+            rightValue = leftValue;
+        }else if(temp == "" && action =='-'){
+
+        }else if(action == '@'){
+            System.out.println("РАВНО");
+        }else rightValue = temp;
+
+
         int numberLeft = Integer.parseInt(leftValue);
         int numberRight = Integer.parseInt(rightValue);
         int answer = 0;
 
         switch(action){
+            case '@':
+                answer = 0;
+                break;
             case '+':
                 answer = numberLeft + numberRight;
                 break;
@@ -88,15 +113,11 @@ class Model {
                 answer = numberLeft - numberRight;
                 break;
         }
+
         temp = String.valueOf(answer);
 
 
-        // ошибка при сложении маленьких чисел!
-//        char a = temp.charAt(temp.length() - 2);
-//        char b = temp.charAt(temp.length() - 1);
-//
-//        if(a == '.' && b == '0'){
-//            temp = temp.substring(0, temp.length()-2);
-//        }
+
+
     }
 }
